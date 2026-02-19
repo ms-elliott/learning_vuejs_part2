0eb7a98131436fa1f7679a88e3444ccea9899556
ms-elliott <ms.elliott55@gmail.com>
2026-02-19 23:52:01 +0900

    finished lessons

--- src/App.vue
+++ src/App.vue
@@ -1,130 +1,158 @@
-<script setup lang="ts">
-// [lang="ts"]と記述するだけでTypeScriptを使用できる様になる
-// import { computed, ref} from 'vue'
+<!-- Options API・・・vue.jsのもう1つのコンポーネントの書き方 -->
+// [data, methods] Options APIでデータを操作する
+<!-- <script setup lang="ts">
+export default {
+  data() {
+    // オブジェクトを返す
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    countUp() {
+      this.count++
+    }
+  }
+}
+</script>
+<template>
+  <h1>Options API</h1>
+  // Composition APIでもOptions APIでもtemplateの記述方法は同じ
+  <p>{{ count }}</p>
+  <button @click="countUp">+1</button>
+</template> -->
 
-// refとTypeScriptを一緒に使う
-// string or number型のrefオブジェクトを定義
-// const count = ref<number | string>(0)
-// count.value = 'hello'
+// [Options API] [Composition API]
+<!-- [Options API]
+・データの書く位置が決まっているので、何も考えなくてもある程度綺麗なコードが書ける
+・リアクティビティシステムを意識する必要がない
+・昔からの機能
+・Composableが使えない
+・TypeScriptと書き方が違い、相性が良くない
+[Composition API]
+・柔軟なので綺麗なコードを書くためには考える必要があるが、最適な位置にコードを置くことができる
+・リアクティビティシステムを意識する必要がない
+・Vue.js 3から登場
+・Composableが使える
+・TypeScriptと書き方がほぼ同じで、相性が良い -->
 
-// computedとTypeScriptを一緒に使う
-// const count = ref(0)
-// 自動的にTypeScriptが型推論する
-// const doubleCount = computed(() => {
-//   if (count.value > 3) return 'hello'
-//   return count.value * 2
-// })
-// doubleCount.value
+// Options APIでの [computed, watch]
+<!-- <script setup lang="ts">
+export default {
+  data() {
+    // オブジェクトを返す
+    return {
+      count: 0,
+    }
+  },
+  computed: {
+    doubleCount() {
+      return this.count * 2
+    }
+  },
+  watch: {
+    count() {
+      console.log('count watcher')
+    }
+  }
+  methods: {
+    countUp() {
+      this.count++
+    }
+  }
+}
+</script>
+<template>
+  <h1>Options API</h1>
+  // Composition APIでもOptions APIでもtemplateの記述方法は同じ
+  <p>{{ count }}</p>
+  <p>doubleCount: {{ doubleCount }}</p>
+  <button @click="countUp">+1</button>
+</template> -->
 
-//useTemlateRefとTypeScriptを一緒に使う
-// import { onMounted, useTemplateRef } from 'vue'
-// 基本的には型推論に任せる。下記のように型指定することも可能
-// const userInput = useTemplateRef<HTMLElement>('user-input')
-// onMounted(() => {
-//   userInput.value?.focus()
-// })
-// userInput.value
-// </script>
-// <template>
-//   <h1>TypeScript</h1>
-//   <input ref="user-input" type="text">
-// </template>
+// Options APIでの [ライフサイクルフック]
+<!-- <script setup lang="ts">
+export default {
+  data() {
+    // オブジェクトを返す
+    return {
+      count: 0,
+    }
+  },
+  computed: {
+    doubleCount() {
+      return this.count * 2
+    }
+  },
+  watch: {
+    count() {
+      console.log('count watcher')
+    }
+  },
+  // watchやmethodsと同じ階層に配置する
+  mounted() {
+    console.log('moumted')
+  }
+  methods: {
+    countUp() {
+      this.count++
+    }
+  }
+}
+</script>
+<template>
+  <h1>Options API</h1>
+  // Composition APIでもOptions APIでもtemplateの記述方法は同じ
+  <p>{{ count }}</p>
+  <p>doubleCount: {{ doubleCount }}</p>
+  <button @click="countUp">+1</button>
+</template> -->
 
+// [props, emit] コンポーネント間のデータの受け渡し
+<!-- App.vue -->
+<!-- <script setup lang="ts">
+import ChildComp from './components/ChildComp.vue'
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      parentCount: 0
+    }
+  }
+}
+</script>
+<template>
+  <h1>Options API</h1>
+  <ChildComp :count="parentCount" @increment="parentCount++" />
+</template> -->
+<!-- ChildComp.vue -->
+<!-- <script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      required: true
+    }
+  },
+  emits: ['increment'],
+  methods: {
+    increment() {
+      this.$emit('increment')
+    }
+  }
+}
+</script>
+<template>
+  <h2>ChildComp</h2>
+  <p>{{ count }}</p>
+  <button @click="increment">+1</button>
+</template> -->
 
-// イベントハンドラーとTypeScriptを一緒に使う
-// import { computed, ref} from 'vue'
-
-// const count = ref(0)
-// const doubleCount = computed(() => {
-//   if (count.value > 3) return 100
-//   return count.value * 2
-// })
-// doubleCount.value
-// イベントオブジェクトを使用する時は、型を明示的に指定する。イベント名をホバーして、payloadの右側の値をコピーし、引数に貼り付ける
-// function countUp(event: PointerEvent) {
-//   count.value++
-//   console.log(event.clientX)
-// }
-// </script>
-// <template>
-//   <h1>TypeScript</h1>
-//   <button @click="countUp">+1</button>
-//   <p>count: {{ count }}</p>
-// </template>
-
-// propsとTypeScriptを一緒に使う方法
-// import { ref } from 'vue'
-// import ChildComp from './components/ChildComp.vue'
-// const courseTitle = ref('Vue.js Course')
-// </script>
-// <template>
-//   <h1>TypeScript</h1>
-//   <ChildComp :title />
-// </template>
-// // ChildComp.vue
-// <script setup lang="ts">
-
-// 下記のdefinePropsの別の記述方法(分割代入)。definePropsのtitleに値が設定されてない場合は、"default course"を設定する。
-// const { title = 'default course' } = defineProps<Course>()
-
-// run time declaration
-// defineProps({
-//   title: {
-//     // vue.jsで型指定して、受け取ることもできる
-//     type: String,
-//     required: true
-//   }
-// })
-// type based declaration
-// defineProps<{
-//   // 型引数を指定することも可能。title?とすることで、required=falseにできる
-//   title: string
-// }>()
-// </script>
-// <template>
-//   <h2>ChildComp</h2>
-//   <p>title: {{ title }}</p>
-// </template>
-
-//defineModel()とTypeScriptを一緒に使う方法
-// import { ref } from 'vue';
-// import ChildComp from './components/ChildComp.vue'
-// const userInput = ref()
-// </script>
-// <template>
-//   <h1>TypeScript</h1>
-//   <ChildComp v-model="userInput" />
-//   <p>userInput: {{ userInput }}</p>
-// </template>
-// // ChildComp.vue
-// <script setup lang="ts">
-// // TypeScriptで型指定する方法
-// const [model, modifiers] = defineModel<string, 'trim' | 'uppercase'>({ required: true })
-// modifiers.uppercase
-// // javaScriptで型指定する方法
-// const model = defineModel({ type: String, required: true })
-// </script>
-// <template>
-//   <h2>ChildComp</h2>
-//   <input v-model="model" type="text">
-//   <p>model: {{ model }}</p>
-// </template>
-
-// emitとtypeScriptを一緒に使う方法
-// import ChildComp from './components/ChildComp.vue'
-// </script>
-// <template>
-//   <h1>TypeScript</h1>
-//   <ChildComp @update=""/>
-// </template>
-// // ChildComp.vue
-// <script setup lang="ts">
-// // defineEmits(['update'])
-// defineEmits<{
-//   update: [count: number]
-// }>()
-// </script>
-// <template>
-//   <h2>ChildComp</h2>
-//   <button @click="$emit('update', 100)">button</button>
-// </template>
+// ビルドツールを使わずにCDNからVue.jsを使う
+<!-- index.html -->
+<!-- // "html:5"と入力すると、HTMLの雛形が呼び出せる
+// htmlをブラウザで見たい場合は、ファイルエクスプローラの当該ファイル上で右クリック->Reveal in Finder->ファイルをクリック
+// vue.jsのwebサイトを開き、"CDNのVueを使用する"にあるコードをコピーし、HTMLのhead内に貼り付ける
+// デプロイ(本番)の時はURLの末尾を書き換えること -->
